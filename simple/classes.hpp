69bfcb30e3e89d965aee8c385e52199669c77e4c
help138 <kmreece2006@gmail.com>
2025-05-26 13:34:59 -0400

    weird noise function(doesn't work)

--- simple/classes.hpp
+++ simple/classes.hpp
@@ -6,15 +6,40 @@ class projectile{
   private:
 
   public:
-    int x, y;
+    float x, y, h, w;
     int speed;
-    projectile(int cx, int cy);
+    projectile(int cx, int cy, int sh, int sw);
     ~projectile(){}
+    // void drawProjectile(unsigned char r, unsigned char g, unsigned char b){
+    //   SDL_SetRenderDrawColor(render, r, g, b, SDL_ALPHA_OPAQUE);  
+    //   SDL_FRect rect = (SDL_FRect){ x, y, h, w};
+    //   SDL_RenderFillRect(render, &rect); 
+    // }
   };
 
-  projectile::projectile(int cx, int cy){
+  projectile::projectile(int cx, int cy, int sh, int sw){
     speed = 0;
-    
+    x = (float)cx;
+    y = (float)cy;
+    h = (float)sh;
+    w = (float)sw;
+  }
+
+  class entity{
+  private:
+
+  public:
+    float x, y;
+    int speed;
+    int hp;
+    entity(/* args */);
+    ~entity(){}
+  };
+  
+  entity::entity(/* args */){
+
   }
 
 #endif
+
+
--- simple/main.cpp
+++ simple/main.cpp
@@ -1,8 +1,9 @@
 #define SDL_MAIN_USE_CALLBACKS 1
-#include "inc/SDL.h"
-#include "inc/SDL_main.h"
+#include <time.h>
 #include <iostream>
+#include "inc/SDL.h"
 #include "classes.hpp" //TODO: move to inc
+#include "inc/SDL_main.h"
 
 #define DEFAULT_WINDOW_WIDTH 640 
 #define DEFAULT_WINDOW_HIGHT 480
@@ -13,22 +14,85 @@ int wind_width = DEFAULT_WINDOW_WIDTH;
 int wind_hight = DEFAULT_WINDOW_HIGHT; 
 SDL_Window* wind = NULL;
 SDL_Renderer* render = NULL;
+bool* stars = NULL;
 
-void draw(){
+
+void test(){
   size_t count;
   count = SDL_GetTicks();
   double now = (double)(count/1000.0);
   const float r = (float)(0.5 + 0.5 * SDL_sin(now));
   const float b = (float)(0.5 + 0.5 * SDL_sin(now + c1));
   const float g = (float)(0.5 + 0.5 * SDL_sin(now + c2));
+
+  SDL_FRect rect;
+
   SDL_SetRenderDrawColorFloat(render, r, g, b, SDL_ALPHA_OPAQUE_FLOAT);  
+  rect.x = 0;    
+  rect.y = 0;    
+  rect.w = wind_width/2;
+  rect.h = wind_hight/2;
+  SDL_RenderFillRect(render, &rect); 
+
+  SDL_SetRenderDrawColorFloat(render, b, r, g, SDL_ALPHA_OPAQUE_FLOAT);  
+  rect.x = wind_width/2;    
+  rect.y = 0;    
+  rect.w = wind_width/2;
+  rect.h = wind_hight/2;
+  SDL_RenderFillRect(render, &rect);
+
+  SDL_SetRenderDrawColorFloat(render, g, b, r, SDL_ALPHA_OPAQUE_FLOAT);  
+  rect.x = 0;
+  rect.y = wind_hight/2;
+  rect.w = wind_width/2;
+  rect.h = wind_hight/2;
+  SDL_RenderFillRect(render, &rect);
+
+  SDL_SetRenderDrawColorFloat(render, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);
+  rect.x = wind_width/2;
+  rect.y = wind_hight/2;
+  rect.w = wind_width/2;
+  rect.h = wind_hight/2;
+  SDL_RenderFillRect(render, &rect);
+}
+void initStars(int freq){
+  SDL_srand(time(NULL) ^ SDL_GetTicksNS());
+  int len = wind_hight * wind_width;
+  stars = (bool*)calloc(sizeof(bool), len);
+  freq = (freq % len);
+  if(freq == 0 || freq == len) freq = len/(SDL_rand(10)+2);
+  for (int i = 0; i < freq; i++) stars[SDL_rand(len)] = 1;
+  
+}
+void template_drawStars(){
+  int len = wind_hight * wind_width;
+  SDL_FPoint arr[len];
+  int count = 0;
+  for (int i = 0; i < len; i++)
+    if(stars[i] == 1) arr[count++] = { (float)(i % wind_width), (float)(i % wind_hight) };
+  SDL_SetRenderDrawColorFloat(render, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);
+  SDL_RenderPoints(render, arr, count);
+}
+void drawStars(){
+  int len = wind_hight * wind_width;
+  for (int i = 0; i < len; i++)
+    if(stars[i] == 1) {
+      SDL_SetRenderDrawColor(render, 255, 255, 255, SDL_ALPHA_OPAQUE);
+      SDL_RenderPoint(render, (float)(i % wind_width), (float)(i % wind_hight));
+    }
+}
+void draw(){
+  SDL_SetRenderDrawColorFloat(render, 0, 0, 0, SDL_ALPHA_OPAQUE_FLOAT);
+
   SDL_RenderClear(render);
   
-
+  drawStars();
 }
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
   SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
   SDL_CreateWindowAndRenderer("callback demo", wind_width, wind_hight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OCCLUDED /*| SDL_WINDOW_MAXIMIZED | SDL_WINDOW_MOUSE_GRABBED | SDL_WINDOW_FULLSCREEN*/, &wind, &render);
+  SDL_srand(time(NULL)); 
+  initStars(100);
   
   if (wind == NULL){
     printf("failed to create window: %s\n", SDL_GetError());
@@ -51,6 +115,9 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
     case SDL_EVENT_WINDOW_RESIZED:
       wind_width = event->window.data1;
       wind_hight = event->window.data2;
+      printf("hight: %d, width: %d, %d\n", wind_hight, wind_width, (wind_hight/wind_width));
+      free(stars);
+      initStars((wind_hight/wind_width)*10);
         break;
     default:
         return SDL_APP_CONTINUE;
@@ -60,12 +127,12 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 SDL_AppResult SDL_AppIterate(void *appstate){
 
   draw();
-  
 
   SDL_RenderPresent(render); 
 
   return SDL_APP_CONTINUE;
 }
 void SDL_AppQuit(void *appstate, SDL_AppResult result){
+  if(stars != NULL) free(stars);
   //SDL will handle most of the cleanup
 }
